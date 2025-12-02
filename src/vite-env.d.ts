102b5ffc7b899569b8cf2bf0f87775fa21563a7b
kaiquemonteoliva <fkaique678@gmail.com>
2025-12-02 11:43:23 -0300

    fix: metadados

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,5 @@
-// Dentro de src/vite-env.d.ts
-
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_SCRIPT_URL: string;
-  // adicione outras variáveis de ambiente que você tiver aqui
 }
